38db6a3e784cb367f02f0e749e73b5dab57ec126
MauricioWiedemann <m.wiedemanngarcia@gmail.com>
2024-08-28 02:36:59 -0300

    ultimos frames

--- src/presentacion/VtAltaClaseDep.java
+++ src/presentacion/VtAltaClaseDep.java
@@ -5,25 +5,101 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
 
 public class VtAltaClaseDep extends JInternalFrame{
-	JButton btnCerrar;
+	JButton btnCancelar;
 	JInternalFrame yo = this;
+	private JTextField textNombre;
+	private JTextField textFecha;
+	private JTextField textHora;
+	private JTextField textLugar;
+	private JTextField textCupos;
+	private JTextField textFechaAlta;
 	public VtAltaClaseDep() {
 		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		this.setBounds(47, 40, 263, 263);
+		this.setBounds(47, 40, 298, 270);
 		this.getContentPane().setLayout(null);
 		this.setTitle("Alta Clase");
 		this.setVisible(false);
 		this.setClosable(false);
 		
-		btnCerrar = new JButton("Cerrar");
-		btnCerrar.addActionListener(new ActionListener() {
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				yo.setVisible(false);
 			}
 		});
-		btnCerrar.setBounds(152, 201, 85, 21);
-		this.getContentPane().add(btnCerrar);
+		btnCancelar.setBounds(167, 208, 105, 21);
+		this.getContentPane().add(btnCancelar);
+		
+		JLabel lblActividad = new JLabel("Actividad");
+		lblActividad.setBounds(10, 21, 105, 14);
+		getContentPane().add(lblActividad);
+		
+		JComboBox cbActividad = new JComboBox();
+		cbActividad.setBounds(125, 17, 147, 22);
+		getContentPane().add(cbActividad);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(10, 46, 105, 14);
+		getContentPane().add(lblNombre);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(125, 43, 147, 20);
+		getContentPane().add(textNombre);
+		textNombre.setColumns(10);
+		
+		JLabel lblFecha = new JLabel("Fecha");
+		lblFecha.setBounds(10, 71, 105, 14);
+		getContentPane().add(lblFecha);
+		
+		JLabel lblHora = new JLabel("Hora");
+		lblHora.setBounds(10, 96, 105, 14);
+		getContentPane().add(lblHora);
+		
+		JLabel lblLugar = new JLabel("Lugar");
+		lblLugar.setBounds(10, 121, 105, 14);
+		getContentPane().add(lblLugar);
+		
+		JLabel lblCupos = new JLabel("Cupos");
+		lblCupos.setBounds(10, 146, 105, 14);
+		getContentPane().add(lblCupos);
+		
+		JLabel lblFechaAlta = new JLabel("Fecha de alta");
+		lblFechaAlta.setBounds(10, 171, 105, 14);
+		getContentPane().add(lblFechaAlta);
+		
+		textFecha = new JTextField();
+		textFecha.setColumns(10);
+		textFecha.setBounds(125, 68, 147, 20);
+		getContentPane().add(textFecha);
+		
+		textHora = new JTextField();
+		textHora.setColumns(10);
+		textHora.setBounds(125, 93, 147, 20);
+		getContentPane().add(textHora);
+		
+		textLugar = new JTextField();
+		textLugar.setColumns(10);
+		textLugar.setBounds(125, 118, 147, 20);
+		getContentPane().add(textLugar);
+		
+		textCupos = new JTextField();
+		textCupos.setColumns(10);
+		textCupos.setBounds(125, 143, 147, 20);
+		getContentPane().add(textCupos);
+		
+		textFechaAlta = new JTextField();
+		textFechaAlta.setColumns(10);
+		textFechaAlta.setBounds(125, 168, 147, 20);
+		getContentPane().add(textFechaAlta);
+		
+		JButton btnConfirmar = new JButton("Confirmar");
+		btnConfirmar.setBounds(52, 207, 105, 21);
+		getContentPane().add(btnConfirmar);
+		
 	}	
 }
--- src/presentacion/VtConsInsc.java
+++ src/presentacion/VtConsInsc.java
@@ -5,25 +5,107 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextField;
 
 public class VtConsInsc extends JInternalFrame{
 	JButton btnCerrar;
 	JInternalFrame yo = this;
+	private JTextField textFechaAlta;
+	private JTextField textCupos;
+	private JTextField textLugar;
+	private JTextField textHora;
+	private JTextField textFecha;
 	public VtConsInsc() {
 		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		this.setBounds(47, 40, 263, 263);
+		this.setBounds(47, 40, 675, 343);
 		this.getContentPane().setLayout(null);
-		this.setTitle("Consulta Inscripcion");
+		this.setTitle("Inscripcion");
 		this.setVisible(false);
 		this.setClosable(false);
-		
+
 		btnCerrar = new JButton("Cerrar");
 		btnCerrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				yo.setVisible(false);
 			}
 		});
-		btnCerrar.setBounds(152, 201, 85, 21);
+		btnCerrar.setBounds(442, 282, 206, 21);
 		this.getContentPane().add(btnCerrar);
+
+		JList listActividades = new JList();
+		listActividades.setBounds(10, 36, 206, 266);
+		getContentPane().add(listActividades);
+
+		JLabel lblActividades = new JLabel("Actividades");
+		lblActividades.setBounds(10, 11, 148, 14);
+		getContentPane().add(lblActividades);
+
+		JList listClases = new JList();
+		listClases.setBounds(226, 36, 206, 138);
+		getContentPane().add(listClases);
+
+		JLabel lblClases = new JLabel("Clases");
+		lblClases.setBounds(226, 11, 148, 14);
+		getContentPane().add(lblClases);
+
+		JLabel lblFecha = new JLabel("Fecha");
+		lblFecha.setBounds(226, 185, 85, 14);
+		getContentPane().add(lblFecha);
+
+		JLabel lblHora = new JLabel("Hora");
+		lblHora.setBounds(226, 210, 85, 14);
+		getContentPane().add(lblHora);
+
+		JLabel lblLugar = new JLabel("Lugar");
+		lblLugar.setBounds(226, 235, 85, 14);
+		getContentPane().add(lblLugar);
+
+		JLabel lblCupos = new JLabel("Cupos");
+		lblCupos.setBounds(226, 260, 85, 14);
+		getContentPane().add(lblCupos);
+
+		JLabel lblFechaAlta = new JLabel("Fecha de alta");
+		lblFechaAlta.setBounds(226, 285, 85, 14);
+		getContentPane().add(lblFechaAlta);
+
+		textFechaAlta = new JTextField();
+		textFechaAlta.setEnabled(false);
+		textFechaAlta.setBounds(332, 282, 100, 20);
+		getContentPane().add(textFechaAlta);
+		textFechaAlta.setColumns(10);
+
+		textCupos = new JTextField();
+		textCupos.setEnabled(false);
+		textCupos.setColumns(10);
+		textCupos.setBounds(332, 257, 100, 20);
+		getContentPane().add(textCupos);
+
+		textLugar = new JTextField();
+		textLugar.setEnabled(false);
+		textLugar.setColumns(10);
+		textLugar.setBounds(332, 232, 100, 20);
+		getContentPane().add(textLugar);
+
+		textHora = new JTextField();
+		textHora.setEnabled(false);
+		textHora.setColumns(10);
+		textHora.setBounds(332, 207, 100, 20);
+		getContentPane().add(textHora);
+
+		textFecha = new JTextField();
+		textFecha.setEnabled(false);
+		textFecha.setColumns(10);
+		textFecha.setBounds(332, 182, 100, 20);
+		getContentPane().add(textFecha);
+
+		JList listInscripciones = new JList();
+		listInscripciones.setBounds(442, 36, 206, 235);
+		getContentPane().add(listInscripciones);
+
+		JLabel lblDeportista = new JLabel("Inscripciones");
+		lblDeportista.setBounds(442, 11, 206, 14);
+		getContentPane().add(lblDeportista);
 	}
 }
